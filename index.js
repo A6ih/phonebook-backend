76a138bfa047e5b1c52deea4411940168815fb48
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-21 21:33:42 +0300

    add eslint and configured eslint also fixed all the errors shown when running npm dev lint

--- index.js
+++ index.js
@@ -1,113 +1,113 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const Person = require("./models/person");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const Person = require('./models/person')
+const app = express()
 
-morgan.token("postData", (request) => JSON.stringify(request.body));
+morgan.token('postData', (request) => JSON.stringify(request.body))
 
-app.use(express.json());
+app.use(express.json())
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :postData"
+    ':method :url :status :res[content-length] - :response-time ms :postData'
   )
-);
-app.use(express.static("dist"));
+)
+app.use(express.static('dist'))
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((result) => {
-    response.json(result);
-  });
-});
+    response.json(result)
+  })
+})
 
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   Person.find({}).then((result) => {
-    const total = result.length;
-    const date = String(new Date());
+    const total = result.length
+    const date = String(new Date())
     response.send(
       `<p>Phonebook has info for ${total} people</p><p>${date}</p>`
-    );
-  });
-});
+    )
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const { name, number } = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const { name, number } = request.body
 
   if (!number) {
     return response.status(400).json({
-      error: "number cannot be empty",
-    });
+      error: 'number cannot be empty',
+    })
   }
 
   const person = new Person({
     name: name,
     number: number,
-  });
+  })
 
   person
     .save()
     .then((newPerson) => {
-      response.json(newPerson);
+      response.json(newPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
   Person.findById(request.params.id)
     .then((person) => {
       if (!person) {
-        return response.status(404).end;
+        return response.status(404).end
       }
-      person.name = name;
-      person.number = number;
+      person.name = name
+      person.number = number
 
       person
         .save()
         .then((updatedPerson) => {
-          response.json(updatedPerson);
+          response.json(updatedPerson)
         })
-        .catch((error) => next(error));
+        .catch((error) => next(error))
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`server started on ${PORT}`);
-});
+  console.log(`server started on ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose
   .connect(url)
-  .then((result) => console.log("Connected to MongoDB"))
+  .then(() => console.log('Connected to MongoDB'))
   .catch((error) => {
-    console.log("Failed to connect:", error.message);
-  });
+    console.log('Failed to connect:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -22,20 +22,20 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function (v) {
-        return /^\d{2,3}-([0-9]+)$/.test(v);
+        return /^\d{2,3}-([0-9]+)$/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
     required: true,
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
